407e0d5fec6301a484d11e9d30fc702bcd5382b4
Kim Tae Seop <rlaxo0306@gmail.com>
2022-11-16 00:36:07 +0900

    ♻️ Refactor: Box 컴포넌트 스타일 보완

--- src/containers/Box/index.jsx
+++ src/containers/Box/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const BoxContainer = styled.div``;
+const BoxContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: 500px;
+`;
 
 const Box = () => {
   return <BoxContainer></BoxContainer>;
